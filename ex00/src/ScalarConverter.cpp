4aae10dfb9750f1dc5c607da145772f3ff472424
Erwann Gache <egache@z4r11p1.42lyon.fr>
2026-03-18 16:22:53 +0100

    ex00 you little shit

--- ex00/src/ScalarConverter.cpp
+++ ex00/src/ScalarConverter.cpp
@@ -1,37 +1,139 @@
 #include "ScalarConverter.hpp"
+#include <cfloat>
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
 
-#define DISPLAYABLE_CHAR                                                       \
-  " !\"#$%'\()*+,-./"                                                          \
-  ":;<=>?@[\\]^_`\{|}~"                                                        \
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
+// #define DISPLAYABLE_CHAR \
+//   " !\"#$%'\()*+,-./" \
+//   ":;<=>?@[\\]^_`\{|}~" \
+//   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890"
+//
+// #define CHAR_TO_CONVERT \
+//   " !\"#$%'\()*+,-./" \
+//   ":;<=>?@[\\]^_`\{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-#define CHAR_TO_CONVERT                                                        \
-  " !\"#$%'\()*+,-./"                                                          \
-  ":;<=>?@[\\]^_`\{|}~abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ScalarConverter::ScalarConverter() {};
 
 ScalarConverter::ScalarConverter(ScalarConverter const &src) { (void)src; }
 
-// static void convertFromChar(char c)
-// {
-//
-// }
-//
-// static void convertFromInt(int i)
-// {
-//
-// }
-//
-// static void convertFromFloat(float f)
-// {
-//
-// }
-//
-// static void convertFromDouble(double d)
-// {
-//
-// }
-//
+static void displayValues(int i, float f, double d)
+{
+  if (i > INT_MIN && i <= INT_MAX)
+    std::cout << "int: " << i << std::endl;
+  else
+    std::cout << "int: impossible" << std::endl;
+  std::cout << "float: " << std::fixed << std::setprecision(1) << f << "f"
+            << std::endl;
+  std::cout << "double: " << std::fixed << std::setprecision(1) << d
+            << std::endl;
+}
+
+static void displayNanValues(void)
+{
+  std::cout << "char: impossible" << std::endl;
+  std::cout << "int: impossible" << std::endl;
+  std::cout << "float: nanf" << std::endl;
+  std::cout << "double: nan" << std::endl;
+}
+
+static void convertFromChar(std::string str)
+{
+  char c;
+  int i;
+  float f;
+  double d;
+
+  c = str[0];
+  if (c >= 32 && c < 127)
+    std::cout << "char: \'" << c << "\'" << std::endl;
+  else if (c >= 0 && c < 32)
+    std::cout << "char: Non displayable" << std::endl;
+  i = static_cast<int>(c);
+  f = static_cast<float>(c);
+  d = static_cast<double>(c);
+  displayValues(i, d, f);
+  return;
+}
+
+static void convertFromInt(std::string str)
+{
+  double tmp;
+
+  tmp = std::strtod(str.c_str(), NULL);
+  if (tmp < INT_MIN || tmp > INT_MAX)
+  {
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: impossible" << std::endl;
+    std::cout << "double: impossible" << std::endl;
+  }
+  else
+  {
+    int i = static_cast<int>(tmp);
+    if (i > CHAR_MAX || i < CHAR_MIN)
+      std::cout << "char: impossible" << std::endl;
+    else if (i >= 32 && i < 127)
+      std::cout << "char: \'" << static_cast<char>(i) << "\'" << std::endl;
+    else if (i >= 0 && i < 32)
+      std::cout << "char: Non displayable" << std::endl;
+    float f = static_cast<float>(i);
+    double d = static_cast<double>(i);
+    displayValues(i, f, d);
+  }
+}
+
+static void convertFromFloat(std::string str)
+{
+  double tmp;
+  float f;
+  int i;
+  double d;
+
+  tmp = std::strtod(str.c_str(), NULL);
+  f = static_cast<float>(tmp);
+  if (isinf(f))
+  {
+    std::cout << "char: impossible" << std::endl;
+    std::cout << "int: impossible" << std::endl;
+    std::cout << "float: " << f << "f" << std::endl;
+    std::cout << "double: " << static_cast<double>(f) << std::endl;
+  }
+  else
+  {
+    if (f > CHAR_MAX || f < CHAR_MIN)
+      std::cout << "char: impossible" << std::endl;
+    else if (f >= 32 && f < 127)
+      std::cout << "char: \'" << static_cast<char>(f) << "\'" << std::endl;
+    else if (f >= 0 && f < 32)
+      std::cout << "char: Non displayable" << std::endl;
+    i = static_cast<int>(f);
+    d = static_cast<double>(f);
+    displayValues(i, f, d);
+  }
+}
+
+static void convertFromDouble(std::string str)
+{
+  double d;
+  int i;
+  float f;
+
+  d = std::strtod(str.c_str(), NULL);
+  if (d > CHAR_MAX || d < CHAR_MIN)
+    std::cout << "char: impossible" << std::endl;
+  else if (d >= 32 && d < 127)
+    std::cout << "char: \'" << static_cast<char>(d) << "\'" << std::endl;
+  else if (d >= 0 && d < 32)
+    std::cout << "char: Non displayable" << std::endl;
+  i = static_cast<int>(d);
+  f = static_cast<float>(d);
+  displayValues(i, f, d);
+}
+
 static size_t charCount(char c, std::string str)
 {
   size_t count = 0;
@@ -44,42 +146,43 @@ static size_t charCount(char c, std::string str)
   return (count);
 }
 
-#include <iostream>
-
 void ScalarConverter::convert(std::string str)
 {
-  if (str.find_first_not_of(DISPLAYABLE_CHAR) != std::string::npos)
+  if (!str.compare("nan") || !str.compare("nanf"))
   {
-    std::cout << "Error\nNot displayable character inside the string";
-  }
-  if (str.find_first_not_of(CHAR_TO_CONVERT) == std::string::npos &&
-      str.length() == 1)
-  {
-    std::cout << "c un char" << std::endl;
+    displayNanValues();
     return;
   }
-  if (str.find_first_not_of("0123456789.f") == std::string::npos)
+  if (str.length() == 1 && str[0] < 48 && str[0] > 57)
   {
-    if (str.find_first_not_of("0123456789.") == std::string::npos)
+    convertFromChar(str);
+    return;
+  }
+  if (str.find_first_of('-', 1) != std::string::npos)
+  {
+    std::cout << "a" << std::endl;
+    return;
+  }
+  if (str.find_first_not_of("-0123456789.f") == std::string::npos)
+  {
+    if (str.find_first_not_of("-0123456789.") == std::string::npos)
     {
-      if (str.find_first_not_of("0123456789") == std::string::npos)
+      if (str.find_first_not_of("-0123456789") == std::string::npos)
       {
-        std::cout << "c un int" << std::endl;
+        convertFromInt(str);
         return;
       }
       if (*str.rbegin() != '.' && charCount('.', str) == 1)
       {
-        std::cout << "c un double" << std::endl;
+        convertFromDouble(str);
         return;
       }
     }
-    std::cout << *str.rbegin() << std::endl;
     if (*str.rbegin() == 'f' && isdigit(*(str.rbegin() + 1)) &&
-        charCount('.', str) == 1 && charCount('f', str))
+        charCount('.', str) == 1 && charCount('f', str) == 1)
     {
-      std::cout << "c un float" << std::endl;
+      convertFromFloat(str);
       return;
     }
   }
-  std::cout << "ya rien" << std::endl;
 }
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,5 +1,5 @@
 #include "ScalarConverter.hpp"
-#include <sstream>
+#include <climits>
 
 int main(int argc, char *argv[])
 {
@@ -8,19 +8,6 @@ int main(int argc, char *argv[])
   std::string str = argv[1];
   if (str.empty())
     return 0;
-  // float f;
-  // double d;
-  // int i;
-  // std::stringstream fstream(str);
-  // std::stringstream dstream(str);
-  // std::stringstream istream(str);
-  // fstream >> f;
-  // dstream >> d;
-  // istream >> i;
-  //
-  // std::cout << f << std::endl;
-  // std::cout << d << std::endl;
-  // std::cout << i << std::endl;
   ScalarConverter::convert(str);
   return 0;
 }
